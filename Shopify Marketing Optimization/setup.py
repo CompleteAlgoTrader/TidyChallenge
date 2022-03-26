75a5d955ec005104e6e7969a2c5cf05daec06e5b
CompleteAlgoTrader <89100315+CompleteAlgoTrader@users.noreply.github.com>
2022-03-25 20:41:32 -0700

    Data Wrangling Shopify

---
+++ Shopify Marketing Optimization/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A data science project about optimizing marketing operations for Shopify stores.',
+    author='Justin Jimenez',
+    license='',
+)
